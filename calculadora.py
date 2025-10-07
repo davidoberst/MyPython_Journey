7478d3d8ef425a301a5c54cbd8084b0c5762421e
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-06 22:28:44 -0500

    Calculator

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,43 @@
-#(+,-,*,/)
 import pyfiglet
-print((pyfiglet.figlet_format("Calculator", font="small")))
+
+print(pyfiglet.figlet_format("Calculator", font="small"))
+
 pos = 0
 temp = ""
-digit1 = [] 
+digit1 = []
 digit2 = []
-userInput = (input("----->  "))
+
+userInput = input("----->  ")
+op = ""
 for i, x in enumerate(userInput):
- if x.isdigit():
-  temp += x
- elif x == "+":
-  if temp:
-   digit1.append(int(temp))
-   temp = ""
-   pos = i
-   break
+    if x.isdigit():
+        temp += x
+    elif x in "+-*/":
+        if temp:
+            digit1.append(int(temp))
+            temp = ""
+        op = x
+        pos = i
+        break
+
 temp2 = ""
 for y in userInput[pos+1:]:
- if y.isdigit():
-  temp2 += y
+    if y.isdigit():
+        temp2 += y
 if temp2:
- digit2.append(int(temp2))
-print(f"La respuesta es : {digit1[0] + digit2[0]}")
+    digit2.append(int(temp2))
+
+if op == "+":
+    r = digit1[0] + digit2[0]
+elif op == "-":
+    r= digit1[0] - digit2[0]
+elif op == "*":
+    r = digit1[0] * digit2[0]
+elif op == "/":
+    r = digit1[0] / digit2[0]
+
+print("Resultado:", r)
+
 
   
  
