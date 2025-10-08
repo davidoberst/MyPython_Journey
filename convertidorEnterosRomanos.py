230f64b5313021a92aa8f439109dbbe91055a17d
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-07 23:15:41 -0500

    ConverterV.0.0

--- convertidorEnterosRomanos.py
+++ convertidorEnterosRomanos.py
@@ -1,15 +1,23 @@
 #Convertidor de Numeros Enteros a Romanos
 
+n = 7   #numero a convertir
 
-values = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
+values = {          #valores de numeros romanos a entero
+    "M": 1000,
     "D": 500,
-    "M": 1000
+    "C": 100,
+    "L": 50,
+    "X": 10,
+    "V": 5,
+    "I": 1
 }
 
-def convert(n):
-    pass
+usingNumber = {}
+
+for simbol, value in values.items():
+    if value <= n:
+     usingNumber[simbol] = value
+     break
+rslt = n - value
+print(rslt)
+    
