5b9b1a4ef48107db77811166fb8a4c4efe45437d
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-15 18:55:52 -0500

    busqueda binaria

--- busquedaBinaria.py
+++ busquedaBinaria.py
@@ -1,26 +1,27 @@
 #Implementar busqueda Binaria
-# = [1, 2, 20, 22, 55, 55, 78, 88, 95, 99]
-
 l = [99,88,1,20,55,2,95,78,55,22]
-def search(l,obj):
-  l = sorted(l)
-  inicio = 0
-  fin = len(l)-1
-  mitad = inicio + fin // 2
-  if obj == l[mitad]:
-     print("Found")
-  while obj > l[mitad]:
-     mitad += 1
-     if obj == l[mitad]:
-        print(f"Object {l[mitad]} founded in position {l.index(l[mitad])}")
-        break   
-  while obj < l[mitad]:
-     mitad -= 1
-     if obj == l[mitad]:
-        print(f"Object {l[mitad]} founded in position {l.index(l[mitad])}")        
-        break
-search(l,2)
 
+def search(l, obj):
+    l = sorted(l)
+    inicio = 0
+    fin = len(l) - 1
+
+    while inicio <= fin:
+        mitad = (inicio + fin) // 2
+
+        if l[mitad] == obj:
+            print(f"Object {l[mitad]} found at position {l.index(l[mitad])}")
+            return
+        elif l[mitad] < obj:
+            inicio = mitad + 1
+        else:
+            fin = mitad - 1
+
+    print("Object not found")
+
+search(l, 55)
+search(l, 2)
+search(l, 95)
 
 
  
