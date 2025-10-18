84f77b8991be1b59b8f5f631969b22d2d7406ca2
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-18 13:42:50 -0500

    Ticket System

---
+++ tickets.py
@@ -0,0 +1,70 @@
+import time
+
+# Creación de tickets
+tickets = []
+ticket_id = 0
+
+
+
+def createTicket():
+    global ticket_id, t
+    ticket = {}
+    print("Create a Ticket:")
+    time.sleep(1)
+    ticket["name"] = input("Name: ")
+    time.sleep(1)
+    ticket["description"] = input("Description: ")
+    time.sleep(1)
+    ticket["priority"] = input("Priority (high, medium, low): ")
+    time.sleep(1)
+    ticket_id += 1
+    ticket["ID"] = ticket_id
+    ticket["Status"] = "Pending"
+    tickets.append(ticket)
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("\n Ticket successfully created!!")
+    op = input("Do you want to create a new ticket? : y/n : ")
+    if op.lower() == "y":
+        createTicket()
+    else:
+        pass
+
+
+def System():
+    
+    print("----- TICKET SYSTEM -----")
+    if not tickets:
+        print("No tickets yet.")
+        
+    else:
+        print("-----------Available Tickets------------ :")
+        for x in tickets:
+            for clave, valor in x.items():
+                print(f"{clave} : {valor}")
+            print("")
+    input("Press enter to go back : ")
+
+
+
+
+
+
+
+
+# Bucle principal
+while True:
+    print("\n-------- Welcome to Tickets -----------")
+    print("Options:")
+    print("1. Create a Ticket")
+    print("2. See Tickets")
+    option = input("Escribe 'exit' para salir o elige una opción (1 o 2): ").strip().lower()
+    if option == "exit":
+        break
+    elif option == "1":
+        createTicket()
+    elif option == "2":
+        System()
+    else:
+        print("Invalid option. Try again.")
