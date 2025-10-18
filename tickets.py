53ba26358da10b61900ad2e0fbe1313992aa9421
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-18 17:12:11 -0500

    Ticket System

--- tickets.py
+++ tickets.py
@@ -1,12 +1,11 @@
 import time
-
 # Creación de tickets
 tickets = []
 ticketsAtended = []
 ticket_id = 0
 
 def createTicket():
-    global ticket_id, t
+    global ticket_id
     ticket = {}
     print("Create a Ticket:")
     time.sleep(1)
@@ -18,7 +17,7 @@ def createTicket():
      priority  = ticket["priority"] = input("Priority (high, medium, low): ")
      time.sleep(1)
      ticket_id += 1
-     ticket["ID"] = ticket_id
+     key = ticket["ID"] = ticket_id
      ticket["Status"] = "Pending"
      if priority.lower() not in ["high","medium","low"]:
         time.sleep(1)
@@ -38,6 +37,20 @@ def createTicket():
         pass
 
 
+
+def FIFO():
+ #search for ID of the Ticket
+ print("")
+ print("\n ------------FIFO------------------")
+ time.sleep(1)
+ print("Searching for Tickets")
+ if not tickets:
+     print("\n No tickets founded...")
+ else:
+     print("\n Tickets founded!")
+     
+        
+
 def System():
     if not tickets:
         print("No tickets yet.")    
@@ -49,22 +62,24 @@ def System():
             print("")
     input("Press enter to go back : ")
 
-def SystemLogic():
- pass
- 
-
-# Bucle principal
 while True:
+    time.sleep(1)
     print("\n-------- Welcome to Tickets -----------")
     print("Options:")
     print("1. Create a Ticket")
     print("2. See Tickets")
-    option = input("\n Escribe 'exit' para salir o elige una opción (1 o 2): ").strip().lower()
+    print("3.Manage Tickets")
+    print("")
+    option = input("\n Escribe 'exit' para salir o elige una opción (1,2, o 3): ").strip().lower()
     if option == "exit":
         break
     elif option == "1":
         createTicket()
     elif option == "2":
         System()
+    elif option == "3":
+        FIFO()
     else:
         print("Invalid option. Try again.")
+
+
