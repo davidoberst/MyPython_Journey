6ff2f44c2838a98310c06ed04a65049a68661b6b
davidoberst <juandavidbustosperez82@gmail.com>
2025-10-22 22:36:57 -0500

    numpy

--- numpy_examples.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-#Crear un Array
-arr = np.array([1, 2, 3, 4, 5])
-print(arr)
-
-#Crear un Array con ceros y unos 
-zeros = np.zeros(10) #el parametro son el numero de ceros que tendra
-print(zeros)
